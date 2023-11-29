34da45b9be1555d2f4cc20b7d44a28282bf44de1
n3ey <vojta.kala.it@gmail.com>
2023-11-29 15:55:36 +0100

    feat(responsive-design): IC-4 add mobile responsive login

---
+++ src/components/utils/TransitionComponent.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+
+const TransitionComponent = ({ children }) => {
+    const [inProp, setInProp] = useState(false);
+
+    useEffect(() => {
+        setInProp(true);
+    }, []);
+
+    return (
+        <CSSTransition in={ inProp } timeout={ 1100 } classNames='a-appear'>
+            { children }
+        </CSSTransition>
+    );
+};
+
+export default TransitionComponent;
