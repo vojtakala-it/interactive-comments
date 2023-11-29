50fd74e2e48d26707ed29dcf47db2924a9fd5081
n3ey <vojta.kala.it@gmail.com>
2023-11-29 12:52:13 +0100

    feat(firebase-setup): IC-3 Add login

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,5 @@ import './index.less'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-        <React.StrictMode>
-            <App/>
-        </React.StrictMode>,
+        <App/>
 )
