1b41557cd1a373117f024d687346af40316d9585
n3ey <vojta.kala.it@gmail.com>
2023-12-01 16:33:34 +0100

    feat(router): IC-5 remove critical vulnerabilities, add custom loader

--- src/components/utils/LoadingComponent.jsx
+++ src/components/utils/LoadingComponent.jsx
@@ -1,20 +1,12 @@
-import { SunspotLoader } from "react-awesome-loaders";
-import TransitionComponent from "../utils/TransitionComponent.jsx";
-
-
 export default function LoadingComponent({ loadingText }) {
 
     return (
         <div className='flexbox flexbox--col flexbox--center-xy h-100vh'>
-            <TransitionComponent>
-                <h1 className='f-medium f-xxl mb-m'>{ loadingText }</h1>
-            </TransitionComponent>
-            <SunspotLoader
-                gradientColors={ ["hsl(250, 44%, 36%)", "hsl(238, 40%, 70%)"] }
-                shadowColor={ "#3730A3" }
-                desktopSize={ "128px" }
-                mobileSize={ "50px" }
-            />
+            <h1 className='f-medium f-xxl mb-m'>{ loadingText }</h1>
+            <div className='loader'>
+                <div className='loader__circle'></div>
+                <div className='loader__circle'></div>
+            </div>
         </div>
     )
 };
