e7e3218c599df494d556bf5da90683561f09dbb8
n3ey <vojta.kala.it@gmail.com>
2023-12-04 16:04:22 +0100

    feat(refactor): IC-6 Refactor CSS, make comments responsive

--- src/components/comments/comment/Comment.jsx
+++ src/components/comments/comment/Comment.jsx
@@ -6,7 +6,7 @@ import HeaderSection from "./header-section/HeaderSection.jsx";
 function Comment() {
 
     return (
-        <div className='comment-section bg-white radius-l flexbox flexbox--col pxy-l r-gap-s'>
+        <div className='comment-body bg-white radius-l flexbox flexbox--col pxy-l r-gap-s'>
             <HeaderSection />
             <p className='f-grayish-blue'>
                 Impressive! Though it seems the drag feature could be improved.
