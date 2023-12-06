0a48f6e12c6d5ab62ef77ba0b4a4458f42d4310b
n3ey <vojta.kala.it@gmail.com>
2023-12-06 12:37:16 +0100

    feat(json-data): IC-6 Add reply data, input for new comments

--- src/components/comments/CommentsSection.jsx
+++ src/components/comments/CommentsSection.jsx
@@ -1,11 +1,14 @@
 import Comment from "./comment/Comment.jsx";
 import TransitionComponent from "../utils/TransitionComponent.jsx";
 import juliusomo from '@/assets/images/avatars/image-juliusomo.png';
+import ramsesmiron from '@/assets/images/avatars/image-ramsesmiron.png';
 import maxblagun from '@/assets/images/avatars/image-maxblagun.png';
+import amyrobson from '@/assets/images/avatars/image-amyrobson.png';
+import AddComment from "./comment/AddComment.jsx";
 
 
 export default function CommentsSection() {
-    const julContent = `
+    const amyContent = `
                         Impressive! Though it seems the drag feature could be improved. 
                         But overall it looks incredible. You've nailed the design and the responsiveness
                         at various breakpoints works really well.
@@ -17,22 +20,57 @@ export default function CommentsSection() {
                         Perhaps you can give me an insight on where I can learn React? Thanks!
     `;
 
+    const julContent = `
+                        I couldn't agree more with this. 
+                        Everything moves so fast and it always seems 
+                        like everyone knows the newest library/framework. 
+                        But the fundamentals are what stay constant.
+    `;
+
+    const ramContent = `
+                        If you're still new, I'd recommend focusing on the fundamentals of HTML,
+                        CSS, and JS before considering React. 
+                        It's very tempting to jump ahead but lay a solid foundation first."
+    `;
+
     return (
-            <TransitionComponent>
-                <div className='comments-section'>
-                    <Comment
-                            avatar={ juliusomo }
-                            userName='juliusomo'
-                            content={ julContent }
-                            createdAt='1 month ago'
-                            score='12'/>
-                    <Comment
-                            avatar={ maxblagun }
-                            userName='maxblagun'
-                            content={ maxContent }
-                            createdAt='2 weeks ago'
-                            score='5'/>
-                </div>
-            </TransitionComponent>
+        <TransitionComponent>
+            <div className='comments-section'>
+                <Comment
+                    avatar={ amyrobson }
+                    userName='amyrobson'
+                    content={ amyContent }
+                    createdAt='1 month ago'
+                    activeUser={ false }
+                    score='12'/>
+                <Comment
+                    avatar={ maxblagun }
+                    userName='maxblagun'
+                    content={ maxContent }
+                    createdAt='2 weeks ago'
+                    activeUser={ false }
+                    score='5'/>
+                <Comment
+                    avatar={ ramsesmiron }
+                    userName='ramsesmiron'
+                    content={ ramContent }
+                    createdAt='1 weeks ago'
+                    activeUser={ false }
+                    isReply={ true }
+                    replyingTo='maxblagun'
+                    score='4'/>
+                <Comment
+                    avatar={ juliusomo }
+                    userName='juliusomo'
+                    content={ julContent }
+                    createdAt='2 days ago'
+                    activeUser={ true }
+                    isReply={ true }
+                    replyingTo='ramsesmiron'
+                    score='2'/>
+                <AddComment
+                    avatar={ juliusomo }/>
+            </div>
+        </TransitionComponent>
     );
 }
--- src/components/comments/comment/btn-section/BtnSection.jsx
+++ src/components/comments/comment/btn-section/BtnSection.jsx
@@ -1,29 +1,41 @@
 import deleteIcon from '@/assets/images/icon-delete.svg';
 import editIcon from '@/assets/images/icon-edit.svg';
+import replyIcon from '@/assets/images/icon-reply.svg';
 import LikesCounter from "../likes-counter/LikesCounter.jsx";
 import { useMediaQuery } from "react-responsive";
 
 
-export default function BtnSection({ score }) {
+export default function BtnSection({ score, isReply }) {
     const isMobile = useMediaQuery({ maxWidth: 568 });
 
     return (
             <div className='comment__comment-btn-section'>
                 { isMobile && <LikesCounter score={ score }/> }
-                <div className='comment__comment-btn-section__state-btns'>
-                    <button className='btn btn--transparent f-red'>
-                        <img className='comment__comment-btn-section__state-btns--delete'
-                             src={ deleteIcon }
-                             alt="An icon of a trash can implying a delete action"/>
-                        Delete
-                    </button>
-                    <button className='btn btn--transparent f-blue'>
-                        <img className='comment__comment-btn-section__state-btns--edit'
-                             src={ editIcon }
-                             alt="An icon of a pencil implying an edit action"/>
-                        Edit
-                    </button>
-                </div>
+                { isReply ?
+                        <button className='btn btn--transparent f-blue'>
+                            <img
+                                    className='comment__comment-btn-section__state-btns--reply'
+                                    src={ replyIcon }
+                                    alt="A backwards arrow implying a reply action"/>
+                            Reply
+                        </button> :
+                        <div className='comment__comment-btn-section__state-btns'>
+                            <button className='btn btn--transparent f-red'>
+                                <img
+                                        className='comment__comment-btn-section__state-btns--delete'
+                                        src={ deleteIcon }
+                                        alt="An icon of a trash can implying a delete action"/>
+                                Delete
+                            </button>
+                            <button className='btn btn--transparent f-blue'>
+                                <img
+                                        className='comment__comment-btn-section__state-btns--edit'
+                                        src={ editIcon }
+                                        alt="An icon of a pencil implying an edit action"/>
+                                Edit
+                            </button>
+                        </div>
+                }
             </div>
     )
 }
